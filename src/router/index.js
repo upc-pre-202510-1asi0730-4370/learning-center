b812e78bb81b9f2bfe3a4bf6f2b602c708cd8f40
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-05-12 20:23:32 -0500

    feat: comment categories route

--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,7 @@ const PageNotFoundComponent = () => import('../public/pages/page-not-found.compo
 const routes = [
     {   path: '/home',                  name: 'home',       component: HomeComponent,               meta: {title: 'Home'}},
     {   path: '/about',                 name: 'about',      component: AboutComponent,              meta: {title: 'About us'}},
-    {   path: '/publishing/categories', name: 'categories', component: CategoryManagementComponent, meta: {title: 'Categories'}},
+   // {   path: '/publishing/categories', name: 'categories', component: CategoryManagementComponent, meta: {title: 'Categories'}},
     {   path: '/',                      name: 'default',    redirect: {name: 'home'}},
     {   path: '/:pathMatch(.*)*',       name: 'not-found',  component: PageNotFoundComponent,       meta: {title: 'Page not found'}},
 ]
